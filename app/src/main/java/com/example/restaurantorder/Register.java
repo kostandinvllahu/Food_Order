b80cb1686079e1e451ac104436f38d47efe21313
kostandinvllahu <Ghostsechackers@gmail.com>
2021-05-18 11:02:53 +0200

    Fetching data from database done, now will make the email verification

--- app/src/main/java/com/example/restaurantorder/Register.java
+++ app/src/main/java/com/example/restaurantorder/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,15 +14,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 EditText mFullName, mEmail, mPassword, mPhone;
 Button mRegisterBtn;
 TextView mLoginBtn;
 FirebaseAuth fAuth;
+FirebaseFirestore fStore;
+String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +46,7 @@ FirebaseAuth fAuth;
         mRegisterBtn = findViewById(R.id.Login);
         mLoginBtn = findViewById(R.id.forgotPassword);
         fAuth = FirebaseAuth.getInstance();
-
+        fStore = FirebaseFirestore.getInstance();
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
@@ -46,6 +57,8 @@ FirebaseAuth fAuth;
             public void onClick(View v) {
                 String password = mPassword.getText().toString().trim();
                 String email = mEmail.getText().toString().trim();
+                String fullName = mFullName.getText().toString();
+                String phone = mPhone.getText().toString();
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required.");
@@ -67,8 +80,24 @@ FirebaseAuth fAuth;
                     public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
                         Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String,Object> user = new HashMap<>();
+                        user.put("fName",fullName);
+                        user.put("email",email);
+                        user.put("phone",phone);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("TAG", "onSuccess: user Profile is created for "+ userID);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("TAG","onFailure: " + e.toString());
+                            }
+                        });
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
                     }else{
                         Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
