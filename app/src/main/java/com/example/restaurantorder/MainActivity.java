19729a5da8ad008744aee002ef88b84454a7927e
kostandinvllahu <Ghostsechackers@gmail.com>
2021-05-24 18:15:36 +0200

    Working on the receipt, i made the program to calculate the total final price that will be uploaded in database

--- app/src/main/java/com/example/restaurantorder/MainActivity.java
+++ app/src/main/java/com/example/restaurantorder/MainActivity.java
@@ -18,13 +18,13 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 
 public class MainActivity extends AppCompatActivity {
-TextView fullName, email, phone;
-TextView select, select2, select3, select4, select5, food1, food2, food3, food4, food5, total, number1, number2, number3, number4, number5, p1, p2, p3, p4, p5;
-Button btnClear, btnOrder, btnSelect1, btnSelect2, btnSelect3, btnSelect4, btnSelect5;
-FirebaseAuth fAuth;
-FirebaseFirestore fStore;
-String userId, e;
-int a, b, c;
+    TextView fullName, email, phone;
+    TextView select, select2, select3, select4, select5, food1, food2, food3, food4, food5, total, number1, number2, number3, number4, number5, p1, p2, p3, p4, p5;
+    Button btnClear, btnOrder, btnSelect1, btnSelect2, btnSelect3, btnSelect4, btnSelect5;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId, e, finalPrice;;
+    int a, b, c, a1, b1, c1, a2, b2, c2, a3, b3, c3, a4, b4, c4, finalP;
 
     @Override
 
@@ -33,7 +33,7 @@ int a, b, c;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // phone = findViewById(R.id.profilePhone);
+        // phone = findViewById(R.id.profilePhone);
         //fullName = findViewById(R.id.profileName);
         //email = findViewById(R.id.profileEmail);
         total = findViewById(R.id.selectcb1);
@@ -79,149 +79,188 @@ int a, b, c;
                 phone.setText(documentSnapshot.getString("phone"));
                 fullName.setText(documentSnapshot.getString("fName"));
                 email.setText(documentSnapshot.getString("email"));
-
-
             }
         });*/
 
-      btnClear.setOnClickListener(new View.OnClickListener() {
+        btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              if(cb1.isChecked())
-                  cb1.setChecked(false);
+                if(cb1.isChecked())
+                    cb1.setChecked(false);
                 number1.setVisibility(View.INVISIBLE);
                 number1.setText("");
-              select.setText("");
-              p1.setText("");
-             if(cb2.isChecked())
-                  cb2.setChecked(false);
+                select.setText("");
+                p1.setText("");
+                if(cb2.isChecked())
+                    cb2.setChecked(false);
                 number2.setVisibility(View.INVISIBLE);
                 number2.setText("");
                 select2.setText("");
                 p2.setText("");
-              if(cb3.isChecked())
-                  cb3.setChecked(false);
+                if(cb3.isChecked())
+                    cb3.setChecked(false);
                 number3.setVisibility(View.INVISIBLE);
                 number3.setText("");
                 select3.setText("");
                 p3.setText("");
-              if(cb4.isChecked())
-                  cb4.setChecked(false);
+                if(cb4.isChecked())
+                    cb4.setChecked(false);
                 number4.setVisibility(View.INVISIBLE);
                 number4.setText("");
                 select4.setText("");
                 p4.setText("");
-              if(cb5.isChecked())
-                  cb5.setChecked(false);
+                if(cb5.isChecked())
+                    cb5.setChecked(false);
                 number5.setVisibility(View.INVISIBLE);
                 number5.setText("");
                 select5.setText("");
                 p5.setText("");
                 total.setText("");
 
-              
+
             }
         });
 
-      btnSelect1.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if(cb1.isChecked()) {
-                  select.append(food1.getText().toString());
-                  number1.setVisibility(View.VISIBLE);
-                  p1.setText("300");
-                  number1.setText("1");
+        btnSelect1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb1.isChecked()) {
+                    select.append(food1.getText().toString());
+                    number1.setVisibility(View.VISIBLE);
+                    p1.setText("300");
+                    number1.setText("1");
 
-              }else{
-                  number1.setVisibility(View.INVISIBLE);
-                  select.setText("");
-                  number1.setText("");
-                  p1.setText("");
-              }
-          }
-      });
+                }else{
+                    number1.setVisibility(View.INVISIBLE);
+                    select.setText("");
+                    number1.setText("");
+                    p1.setText("");
+                }
+            }
+        });
 
 
-      btnSelect2.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if(cb2.isChecked()) {
-                  select2.append(food2.getText().toString());
-                  number2.setVisibility(View.VISIBLE);
-                  p2.setText("500");
-                  number2.setText("1");
-              }else{
-                  number2.setVisibility(View.INVISIBLE);
-                  select2.setText("");
-                  number2.setText("");
-                  p2.setText("");
-              }
-          }
-      });
+        btnSelect2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb2.isChecked()) {
+                    select2.append(food2.getText().toString());
+                    number2.setVisibility(View.VISIBLE);
+                    p2.setText("500");
+                    number2.setText("1");
+                }else{
+                    number2.setVisibility(View.INVISIBLE);
+                    select2.setText("");
+                    number2.setText("");
+                    p2.setText("");
+                }
+            }
+        });
 
-      btnSelect3.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if(cb3.isChecked()) {
-                  select3.append(food3.getText().toString());
-                  number3.setVisibility(View.VISIBLE);
-                  p3.setText("450");
-                  number3.setText("1");
-              }else{
-                  number3.setVisibility(View.INVISIBLE);
-                  select3.setText("");
-                  number3.setText("");
-                  p3.setText("");
-              }
-          }
-      });
+        btnSelect3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb3.isChecked()) {
+                    select3.append(food3.getText().toString());
+                    number3.setVisibility(View.VISIBLE);
+                    p3.setText("450");
+                    number3.setText("1");
+                }else{
+                    number3.setVisibility(View.INVISIBLE);
+                    select3.setText("");
+                    number3.setText("");
+                    p3.setText("");
+                }
+            }
+        });
 
-      btnSelect4.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if(cb4.isChecked()) {
-                  select4.append(food4.getText().toString());
-                  number4.setVisibility(View.VISIBLE);
-                  p4.setText("200");
-                  number4.setText("1");
-              }else{
-                  number4.setVisibility(View.INVISIBLE);
-                  select4.setText("");
-                  number4.setText("");
-                  p4.setText("");
-              }
-          }
-      });
+        btnSelect4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb4.isChecked()) {
+                    select4.append(food4.getText().toString());
+                    number4.setVisibility(View.VISIBLE);
+                    p4.setText("200");
+                    number4.setText("1");
+                }else{
+                    number4.setVisibility(View.INVISIBLE);
+                    select4.setText("");
+                    number4.setText("");
+                    p4.setText("");
+                }
+            }
+        });
 
-      btnSelect5.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if(cb5.isChecked()) {
-                  select5.append(food5.getText().toString());
-                  number5.setVisibility(View.VISIBLE);
-                  p5.setText("600");
-                  number5.setText("1");
-              }else{
-                  number5.setVisibility(View.INVISIBLE);
-                  select5.setText("");
-                  number5.setText("");
-                  p5.setText("");
-              }
-          }
-      });
-      btnOrder.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-            total.append(select.getText().toString() + " x " + number1.getText().toString() +  select2.getText().toString() + " x " + number2.getText().toString() + select3.getText().toString() + " x " + number3.getText().toString() + select4.getText().toString() + " x " + number4.getText().toString() + " x " + number5.getText().toString() + select5.getText().toString());
-           // test.append(number11.getText().toString());
-              a = Integer.parseInt(p1.getText().toString());
-              b = Integer.parseInt(number1.getText().toString());
-              c = a * b;
-              e = String.valueOf(c);
-              p1.setText(e);
+        btnSelect5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb5.isChecked()) {
+                    select5.append(food5.getText().toString());
+                    number5.setVisibility(View.VISIBLE);
+                    p5.setText("600");
+                    number5.setText("1");
+                }else{
+                    number5.setVisibility(View.INVISIBLE);
+                    select5.setText("");
+                    number5.setText("");
+                    p5.setText("");
+                }
+            }
+        });
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-          }
-      });
+                //CheckBox1
+                if(cb1.isChecked()){
+                    a = Integer.parseInt(p1.getText().toString());
+                    b = Integer.parseInt(number1.getText().toString());
+                    c = a * b;
+                    e = String.valueOf(c);
+                    p1.setText(e);
+                }
+
+                //CheckBox2
+               if(cb2.isChecked()) {
+                   a1 = Integer.parseInt(p2.getText().toString());
+                   b1 = Integer.parseInt(number2.getText().toString());
+                   c1 = a1 * b1;
+                   e = String.valueOf(c1);
+                   p2.setText(e);
+               }
+                //CheckBox3
+                if(cb3.isChecked()){
+                    a2 = Integer.parseInt(p3.getText().toString());
+                    b2 = Integer.parseInt(number3.getText().toString());
+                    c2 = a2 * b2;
+                    e = String.valueOf(c2);
+                    p3.setText(e);
+                }
+
+                //CheckBox4
+                if(cb4.isChecked()){
+                    a3 = Integer.parseInt(p4.getText().toString());
+                    b3 = Integer.parseInt(number4.getText().toString());
+                    c3 = a3 * b3;
+                    e = String.valueOf(c3);
+                    p4.setText(e);
+                }
+
+                //CheckBox5
+                if(cb5.isChecked()){
+                    a4 = Integer.parseInt(p5.getText().toString());
+                    b4 = Integer.parseInt(number5.getText().toString());
+                    c4 = a4 * b4;
+                    e = String.valueOf(c4);
+                    p5.setText(e);
+                }
+                finalP = c + c1 + c2 + c3 + c4;
+                finalPrice = String.valueOf(finalP);
+               total.append(finalPrice.toString());
+              //total.append(select.getText().toString() + " x " + number1.getText().toString() +  select2.getText().toString() + " x " + number2.getText().toString() + select3.getText().toString() + " x " + number3.getText().toString() + select4.getText().toString() + " x " + number4.getText().toString() + " x " + number5.getText().toString() + select5.getText().toString() + finalPrice.toString());
+                // test.append(number11.getText().toString());
+            }
+        });
     }
     public void logout(View view){
         FirebaseAuth.getInstance().signOut();
