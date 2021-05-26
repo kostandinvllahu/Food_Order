be4794de5e4cbdb001d65c5b675f504a5b60d506
kostandinvllahu <Ghostsechackers@gmail.com>
2021-05-26 16:53:11 +0200

    Fragment working

--- app/src/main/java/com/example/restaurantorder/Order.java
+++ app/src/main/java/com/example/restaurantorder/Order.java
@@ -3,18 +3,24 @@ package com.example.restaurantorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class Order extends AppCompatActivity {
-Button sub;
+Button del;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-   getSupportFragmentManager().beginTransaction().add(R.id.delivery,new Delivery()).commit();
-
-
+        del =findViewById(R.id.submit);
+        del.setOnClickListener(this::onClick);
+ //  getSupportFragmentManager().beginTransaction().add(R.id.delivery,new Delivery()).commit();
+    }
+    public void onClick(View v){
+        if(v.getId()==R.id.submit){
+            getSupportFragmentManager().beginTransaction().add(R.id.delivery,new Delivery()).commit();
+        }
     }
 }
