93527e4fd277371807b88acb18d0cafed816a6e5
kostandinvllahu <Ghostsechackers@gmail.com>
2021-05-27 12:14:16 +0200

    Both fragments are working i separate the screen and now will add the API google map

--- app/src/main/java/com/example/restaurantorder/Order.java
+++ app/src/main/java/com/example/restaurantorder/Order.java
@@ -3,21 +3,14 @@ package com.example.restaurantorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.strictmode.CleartextNetworkViolation;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Order extends AppCompatActivity {
     Button del, take;
@@ -43,14 +36,18 @@ public class Order extends AppCompatActivity {
 
     public void onClick(View v) {
         if (v.getId() == R.id.submit) {
+            FrameLayout layoutOne = (FrameLayout)findViewById(R.id.takeaway);
+            layoutOne.setVisibility(View.INVISIBLE);
             FrameLayout layout = (FrameLayout)findViewById(R.id.delivery);
             layout.setVisibility(View.VISIBLE);
             getSupportFragmentManager().beginTransaction().add(R.id.delivery, new Delivery()).commit();
         }
         if(v.getId() == R.id.submit2){
-            FrameLayout layout = (FrameLayout)findViewById(R.id.delivery);
-            layout.setVisibility(View.INVISIBLE);
-          //  getSupportFragmentManager().beginTransaction().add(R.id.delivery, new Take_Away()).commit();
+            FrameLayout layoutOne = (FrameLayout)findViewById(R.id.delivery);
+            layoutOne.setVisibility(View.INVISIBLE);
+            FrameLayout layout = (FrameLayout)findViewById(R.id.takeaway);
+            layout.setVisibility(View.VISIBLE);
+            getSupportFragmentManager().beginTransaction().add(R.id.takeaway, new Take_Away()).commit();
         }
     }
     }
